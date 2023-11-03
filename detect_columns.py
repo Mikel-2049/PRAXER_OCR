b7c3feccc3b9f49d111146007b7d5c41187fd5f2
Mikel <mikelamg27@gmail.com>
2023-11-02 18:11:09 -0600

    Added mechanism to automatically detect and clasify columns based on position

--- detect_columns.py
+++ detect_columns.py
@@ -1,1 +1,20 @@
- 
+def detect_columns(cells):
+    # Find the unique x-coordinates of the cell boundaries
+    x_boundaries = sorted({cell[0] for cell in cells})
+    
+    # Initialize the list of columns
+    columns = [[] for _ in range(len(x_boundaries) - 1)]
+    
+    # Assign each cell to a column based on its x-coordinate
+    for cell in cells:
+        for i in range(len(x_boundaries) - 1):
+            left = x_boundaries[i]
+            right = x_boundaries[i + 1]
+            if left <= cell[0] < right:
+                columns[i].append(cell)
+                break
+    
+    return columns
+
+
+
