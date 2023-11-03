406851da376e9c2bfc2028176be553b09dc2e3b5
Mikel <mikelamg27@gmail.com>
2023-11-02 18:12:54 -0600

    Functionality to create the dataframe for data storage and extraction of ITEM column

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import pytesseract as PT
 from detect_cells import find_cells
+from detect_columns import detect_columns
+from process_item import read_item
+
 
 def main():
-    image = 'Images\ReTabla_50024D_prueba_2.png'
-    contours, num_rows, num_columns = find_cells(image)
+    image = 'Images\Tabla_50024D_prueba_1.png'
+    cell_regions, num_rows, num_columns = find_cells(image)
+    columns = detect_columns(cell_regions)
+    item_column = read_item(image, columns[0])
 
 if __name__ == "__main__":
     main()
--- process_item.py
+++ process_item.py
@@ -1,1 +1,42 @@
- 
+import pandas as pd
+import cv2
+from pytesseract import pytesseract as PT
+
+PT.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract'
+
+def read_item(image_path, column):
+    img = cv2.imread(image_path)
+    
+    data = []
+    for i, (x, y, w, h) in enumerate(column):
+        # Extract region corresponding to current cell
+        cell = img[y:y+h, x:x+w]
+        
+        # Extract text from cell using OCR
+        text = PT.image_to_string(cell)
+        text = text.strip()
+        
+        # Process text
+        if i == 0:
+            if text != 'ITEM':
+                raise ValueError('First cell should contain "ITEM"')
+        else:
+            if not text or text.isspace():
+                data.append(i)  # Append index if cell is empty
+            else:
+                try:
+                    number = int(text)
+                except ValueError:
+                    raise ValueError(f'Cell {i} should contain a number or be empty')
+                data.append(number)
+    
+    # Create DataFrame
+    df = pd.DataFrame(data, columns=['ITEM'])
+    
+    return df
+
+
+
+
+
+
