3d9a79918c0b611e6167e7dae579ca3b6c5d1510
Mikel <mikelamg27@gmail.com>
2023-11-06 19:02:03 -0600

    Created a semi functional process_tag file

--- process_tag.py
+++ process_tag.py
@@ -1,1 +1,45 @@
- 
+from utils import is_similar
+
+import pandas as pd
+import cv2
+from pytesseract import pytesseract as PT
+
+
+PT.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract'
+
+def preprocess_image(cell):
+    # Apply preprocessing to the cell image to improve OCR accuracy
+    # This might include thresholding, noise removal, etc.
+    # For now, we will just return the cell image as is
+    return cell
+
+def read_tag(image_path, column, num_rows):
+    img = cv2.imread(image_path)
+    data = []
+    
+    # Ensure the first cell is what you expect, but do not add to data
+    first_cell = column[0]
+    first_cell_image = img[first_cell[1]:first_cell[1]+first_cell[3], first_cell[0]:first_cell[0]+first_cell[2]]
+    first_cell_text = PT.image_to_string(first_cell_image).strip()
+    #print(first_cell_text)
+    if not is_similar(first_cell_text, 'TAG', threshold=80):  # 80 is just an example threshold
+        raise ValueError(f'First cell does not contain the expected title, got {first_cell_text} instead')
+
+    for i, (x, y, w, h) in enumerate(column):
+        if i == 0:  # Skip the title cell after the check
+            continue
+        
+        # Extract the region corresponding to the current cell
+        cell = img[y:y+h, x:x+w]
+        # Preprocess the image (if necessary)
+        processed_cell = preprocess_image(cell)
+        # Extract text from the cell using OCR
+        text = PT.image_to_string(processed_cell).strip()
+        # Append the text to the data list
+        data.append(text)
+
+    # Since the first cell was skipped, the index should be offset by 1
+    df = pd.DataFrame(data, columns=['TAG'])
+    print(df)
+
+    return df
