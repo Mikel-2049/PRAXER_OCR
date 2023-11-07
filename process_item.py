184de40e7f09b248bd2b761dfc39a3909efcbe90
Mikel <mikelamg27@gmail.com>
2023-11-06 20:17:13 -0600

    improved functions for compatibility and clarity

--- process_item.py
+++ process_item.py
@@ -1,38 +1,40 @@
+from utils import is_similar
+
 import pandas as pd
 import cv2
 from pytesseract import pytesseract as PT
 
+
 PT.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract'
+
 
 def read_item(image_path, column):
     img = cv2.imread(image_path)
-    
     data = []
     for i, (x, y, w, h) in enumerate(column):
-        # Extract region corresponding to current cell
         cell = img[y:y+h, x:x+w]
-        
-        # Extract text from cell using OCR
-        text = PT.image_to_string(cell)
-        text = text.strip()
-        
-        # Process text
+        text = PT.image_to_string(cell).strip()
+        #print(f"Cell {i}: '{text}'")
+
         if i == 0:
-            if text != 'ITEM':
-                raise ValueError('First cell should contain "ITEM"')
+            # Use similarity check for the first cell
+            if not is_similar(text, 'ITEM'):
+                raise ValueError('First cell should contain "ITEM"', text)
         else:
             if not text or text.isspace():
-                data.append(i)  # Append index if cell is empty
+                # Append index if cell is empty
+                data.append(i)
             else:
                 try:
                     number = int(text)
+                    data.append(number)
                 except ValueError:
-                    raise ValueError(f'Cell {i} should contain a number or be empty')
-                data.append(number)
+                    # Instead of raising an error, append the index if the cell text is not a number
+                    data.append(i)
     
-    # Create DataFrame
+    # Create DataFrame with the 'ITEM' column
     df = pd.DataFrame(data, columns=['ITEM'])
-    
+    print(df)
     return df
 
 
--- process_tag.py
+++ process_tag.py
@@ -7,39 +7,49 @@ from pytesseract import pytesseract as PT
 
 PT.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract'
 
-def preprocess_image(cell):
-    # Apply preprocessing to the cell image to improve OCR accuracy
-    # This might include thresholding, noise removal, etc.
-    # For now, we will just return the cell image as is
-    return cell
+def preprocess_image(img, coords):
+    # Extract the region of interest using the coordinates
+    x, y, w, h = coords
+    cell = img[y:y+h, x:x+w]
+
+    # Convert to grayscale
+    gray = cv2.cvtColor(cell, cv2.COLOR_BGR2GRAY)
+
+    # Thresholding to get a binary image
+    _, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+
+    # Resize the image to make it larger for better OCR
+    scale_factor = 2.5  # Example: Make the image double its size
+    width = int(w * scale_factor)
+    height = int(h * scale_factor)
+    dim = (width, height)
+
+    # Perform the actual resizing of the image
+    resized = cv2.resize(thresh, dim, interpolation=cv2.INTER_LINEAR)
+
+    return resized
+
 
 def read_tag(image_path, column, num_rows):
     img = cv2.imread(image_path)
     data = []
-    
-    # Ensure the first cell is what you expect, but do not add to data
-    first_cell = column[0]
-    first_cell_image = img[first_cell[1]:first_cell[1]+first_cell[3], first_cell[0]:first_cell[0]+first_cell[2]]
-    first_cell_text = PT.image_to_string(first_cell_image).strip()
-    #print(first_cell_text)
-    if not is_similar(first_cell_text, 'TAG', threshold=80):  # 80 is just an example threshold
-        raise ValueError(f'First cell does not contain the expected title, got {first_cell_text} instead')
 
-    for i, (x, y, w, h) in enumerate(column):
+    for i, coords in enumerate(column):
         if i == 0:  # Skip the title cell after the check
             continue
         
-        # Extract the region corresponding to the current cell
-        cell = img[y:y+h, x:x+w]
-        # Preprocess the image (if necessary)
-        processed_cell = preprocess_image(cell)
-        # Extract text from the cell using OCR
-        text = PT.image_to_string(processed_cell).strip()
+        # Preprocess the image (including resizing) using the coordinates
+        processed_cell = preprocess_image(img, coords)
+
+        custom_config = r'-c tessedit_char_whitelist=ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789- --psm 6'
+
+        text = PT.image_to_string(processed_cell, config=custom_config).strip()
+
         # Append the text to the data list
         data.append(text)
 
-    # Since the first cell was skipped, the index should be offset by 1
     df = pd.DataFrame(data, columns=['TAG'])
     print(df)
 
     return df
+
