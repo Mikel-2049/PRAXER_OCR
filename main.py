feaf5d2e755d5c0a568ece4707341aa81468c158
Mikel <mikelamg27@gmail.com>
2023-11-07 13:38:43 -0600

    Updateed main file to include new functions

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from detect_columns import detect_columns
 from process_item import read_item
 from process_item_code import read_item_code
 from process_tag import read_tag
+from process_quantity import read_quantity
+from process_nps import read_nps
 
 
 def main():
@@ -13,6 +15,8 @@ def main():
     item_column = read_item(image, columns[0])
     item_code_column = read_item_code(image, columns[1], num_rows)
     tag_column = read_tag(image, columns[2], num_rows)
+    quantity_column = read_quantity(image, columns[3], num_rows)
+    nps_column = read_nps(image, columns[4], num_rows)
 
 if __name__ == "__main__":
     main()
